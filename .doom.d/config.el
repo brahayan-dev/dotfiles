257106ed25abff43031e0659b93d3700997b0124
Brahayan Xavier <brahayanxavier@Brahayans-MacBook-Pro.local>
2023-09-12 09:09:35 -0500

    feat: add leader key

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -5,7 +5,7 @@
       user-mail-address "brahayan@live.com")
 
 ;; Theme:
-(setq doom-theme 'doom-one-light)
+(setq doom-theme 'doom-ayu-dark)
 (setq display-line-numbers-type 'relative)
 (setq org-directory "~/Projects/writtings/")
 (setq doom-font (font-spec :family "Fira Code" :size 18)
@@ -14,10 +14,11 @@
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
 
 ;; Behavior:
-(setq project-enable-caching nil
+(setq doom-localleader-key ","
+      project-enable-caching nil
       projectile-project-search-path '("~/Projects"))
-(use-package! lsp-mode                                                    
-  :commands lsp                                                           
-  :config                                                                 
-  (setq lsp-semantic-tokens-enable t)                                     
+(use-package! lsp-mode
+  :commands lsp
+  :config
+  (setq lsp-semantic-tokens-enable t)
   (add-hook 'lsp-after-apply-edits-hook (lambda (&rest _) (save-buffer))))
