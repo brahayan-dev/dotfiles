b0276f27f5a01a016c162432c6192029584f284f
Brahayan Xavier <brahayan@live.com>
2024-06-22 11:29:14 -0500

    feat: add emacs vanilla

--- doom/init.el
+++ doom/init.el
@@ -81,8 +81,10 @@
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       grammar             ; tasing grammar mistake every you make
-       (spell +flyspell)   ; tasing you for misspelling mispelling
+       ;; grammar          ; tasing grammar mistake every you make
+       ;; (spell +enchant
+       ;;        +flyspell
+       ;;        +everywhere) ; tasing you for misspelling mispelling
 
        :tools
        ;;ansible
@@ -145,7 +147,7 @@
        (javascript +lsp +tree-sitter) ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia                        ; a better, faster MATLAB
        ;;kotlin                       ; a better, slicker Java(Script)
-       (latex +lsp +latexmk)          ; writing papers in Emacs has never been so fun
+       ;; (latex +lsp +latexmk)       ; writing papers in Emacs has never been so fun
        ;;lean                         ; for folks with too much to prove
        ;;ledger                       ; be audit you can be
        ;;lua                          ; one-based indices? one-based indices
---
+++ emacs/init.el
@@ -0,0 +1,3 @@
+(set-face-attribute 'default nil :font "JetBrains Mono-22")
+(setq inhibit-startup-message t
+      visible-bell t)  
