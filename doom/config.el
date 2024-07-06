84de381a761b66aa2884a745f04f48a058f75a19
Brahayan Xavier <brahayan@live.com>
2024-07-06 09:22:23 -0500

    feat: update config

--- doom/config.el
+++ doom/config.el
@@ -1,33 +1,66 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Light themes:
+;; ---------
+;; Themes
+;; ---------
 (setq doom-theme 'doom-nord-light)
-;; (setq doom-theme 'doom-bluloco-light)
-;; (setq doom-theme 'doom-gruvbox-light)
-
-;; Dark themes:
-;; (setq doom-theme 'doom-dracula)
-;; (setq doom-theme 'doom-ayu-dark)
 ;; (setq doom-theme 'doom-miramare)
 
-;; Configuration:
-;; (setq display-line-numbers-type 't)
-(setq display-line-numbers-type 'relative)
-(setq org-directory "~/Projects/writtings/")
-(setq doom-font (font-spec :family "JetBrains Mono" :size 18)
-      doom-symbol-font doom-font
-      read-process-output-max (* 1024 1024))
-(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+;; ------
+;; Doom
+;; ------
+(setq doom-symbol-font doom-font
+      doom-localleader-key ","
+      doom-font (font-spec :family "JetBrains Mono" :size 18))
 
-;; Behavior:
-(setq doom-localleader-key ","
+;; ---------
+;; Editor
+;; ---------
+(if (fboundp 'toggle-frame-fullscreen) (toggle-frame-fullscreen))
+(setq no-byte-compile nil
       project-enable-caching nil
-      no-byte-compile nil
-      projectile-project-search-path '("~/Projects"))
+      frame-resize-pixelwise t
+      package-native-compile t
+      display-line-numbers-type 'relative
+      org-directory "~/Projects/workbooks/"
+      read-process-output-max (* 1024 1024)
+      projectile-project-search-path '("~/Projects")
+      backup-directory-alist `(("." . ,(expand-file-name ".tmp/backups/" user-emacs-directory))))
 
+;; ------
+;; Hook
+;; ------
+(add-hook 'before-save-hook #'whitespace-cleanup)
+
+;; ---------
+;; Latex
+;; ---------
+(setq-default TeX-master nil)
+(setq TeX-auto-save t
+      TeX-parse-self t
+      font-latex-fontify-script nil
+      font-latex-fontify-sectioning 1.0
+      font-latex-fontify-sectioning 'color)
+
+;; ------
+;; Lsp
+;; ------
 (use-package! lsp-mode
+  :ensure t
   :commands lsp
   :config
   (setq lsp-semantic-tokens-enable t)
   (add-hook 'lsp-after-apply-edits-hook
             (lambda (&rest _) (save-buffer))))
+
+;; ---------
+;; Paredit
+;; ---------
+(use-package! paredit
+  :diminish
+  :ensure t
+  ;; Bind RET to nil, to fix Cider REPL buffer eval issue
+  :bind (:map paredit-mode-map ("RET" . nil))
+  :hook ((clojure-mode . paredit-mode)
+         (emacs-lisp-mode . paredit-mode)
+         (clojurescript-mode . paredit-mode)))
