8e7bb37d65e43920db4f0a2dd78c4290b10fe2ab
Brahayan <bxsr@proton.me>
2025-04-11 21:53:41 -0500

    feat: add fennel

---
+++ .config/nvim/fnl/config/init.fnl
@@ -0,0 +1,6 @@
+(local {: autoload} (require :nfnl.module))
+(local core (autoload :nfnl.core))
+
+(core.assoc vim.o :number true)
+
+{}
---
+++ .config/nvim/lua/config/init.lua
@@ -0,0 +1,6 @@
+-- [nfnl] Compiled from fnl/config/init.fnl by https://github.com/Olical/nfnl, do not edit.
+local _local_1_ = require("nfnl.module")
+local autoload = _local_1_["autoload"]
+local core = autoload("nfnl.core")
+core.assoc(vim.o, "number", true)
+return {}
