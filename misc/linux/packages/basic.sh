d5edaebd67424e589d60f6c1b2a1cf2a34e4063f
Xavier Ramirez <xavier.ramirez@nubank.com.br>
2025-05-12 14:31:23 -0500

    test: add extra tasks

--- misc/linux/packages/basic.sh
+++ misc/linux/packages/basic.sh
@@ -1,48 +1,7 @@
 #!/usr/bin/env bash
 
-sudo dnf copr enable birkch/dbeaver
-sudo dnf install dbeaver-ce 
-sudo dnf copr enable pgdev/ghostty
-sudo dnf install ghostty
-
-# Fonts
-mkdir -p /tmp/adodefont
-cd /tmp/adodefont
-mkdir -p ~/.fonts
-wget https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.050R-it.zip
-unzip 1.050R-it.zip
-cp source-code-pro-2.030R-ro-1.050R-it/OTF/*.otf ~/.fonts/
-wget https://github.com/adobe-fonts/source-serif-pro/archive/2.000R.zip
-unzip 2.000R.zip
-cp source-serif-pro-2.000R/OTF/*.otf ~/.fonts/
-wget https://github.com/adobe-fonts/source-sans-pro/archive/2.020R-ro/1.075R-it.zip
-unzip 1.075R-it.zip
-cp source-sans-pro-2.020R-ro-1.075R-it/OTF/*.otf ~/.fonts/
-fc-cache -f -v
-
-# Babashka
-curl -o babashka -sLO https://raw.githubusercontent.com/babashka/babashka/master/install
-chmod +x babashka
-sudo bash ./babashka
-rm babashka
-# Cljfmt
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/weavejester/cljfmt/HEAD/install.sh)"
-# Clojure Lsp
-sudo bash < <(curl -s https://raw.githubusercontent.com/clojure-lsp/clojure-lsp/master/install)
 
 # Define default shell
 chsh -s "$(which zsh)"
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Install doom
-git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
-~/.config/emacs/bin/doom install
-
-# Change emacs icon
-mkdir -p "$HOME/.local/share/icons"
-ICON="$HOME/.local/share/icons/doom.png"
-DESKTOP_FILE=/usr/share/applications/emacs.desktop
-wget https://raw.githubusercontent.com/eccentric-j/doom-icon/master/cute-doom/doom.png -O "$ICON" &&
-    sudo --preserve-env=ICON,DESKTOP_FILE sed -i "s|Icon=.*|Icon=$ICON|" $DESKTOP_FILE
-
