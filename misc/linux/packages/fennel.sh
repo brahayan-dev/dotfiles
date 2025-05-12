f4b8324b951db0295b1ed95e750e933c8ff13f3d
Xavier Ramirez <xavier.ramirez@nubank.com.br>
2025-05-12 14:41:53 -0500

    chore: clean up!

--- misc/linux/packages/fennel.sh
+++ misc/linux/packages/fennel.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/env sh
 
 # -----------
-# Lua
-# -----------
-cd /tmp/
-curl -L -R -O https://www.lua.org/ftp/lua-5.4.7.tar.gz
-tar zxf lua-5.4.7.tar.gz
-cd lua-5.4.7
-make all test
-mkdir -p ~/.local/bin
-mkdir -p ~/.local/include/lua54
-mv src/* ~/.local/include/lua54
-mv ~/.local/include/lua54/lua ~/.local/bin
-
-# -----------
 # Luarocks
 # -----------
 cd /tmp/
@@ -22,21 +9,5 @@ tar zxpf luarocks-3.11.1.tar.gz
 cd luarocks-3.11.1
 ./configure && make && sudo make install
 
-# -----------
-# Fennel
-# -----------
-sudo luarocks install fennel
-
-# -----------------------------
-# Fennel LS and Fnlfmt
-# -----------------------------
-cd /tmp
-git clone https://git.sr.ht/~xerool/fennel-ls
-cd fennel-ls
-sudo make --force install
-cd ..
-# git clone https://git.sr.ht/~technomancy/fnlfmt
-# cd fnlfmt
-# sudo make --force install
 
 
