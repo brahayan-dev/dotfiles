bc9121fdaf9d75f878b70ef48302c43440518d2a
Brahayan Suarez <bxsr@proton.me>
2025-06-10 17:18:31 -0500

    feat: add ruby

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -169,7 +169,11 @@
        ;;raku                         ; the artist formerly known as perl6
        ;;rest                         ; Emacs as a REST client
        ;;rst                          ; ReST in peace
-       ;;(ruby +rails)                ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       (when (equal system-type 'gnu/linux)
+         (ruby +lsp
+               +rbenv
+               +rails
+               +tree-sitter))         ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;(rust +lsp)                  ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala                        ; java, but good
        ;;(scheme +chez)               ; a fully conniving family of lisps
