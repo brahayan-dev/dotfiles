8b75b5627609b90435ccf58c29555efd59843539
Brahayan Xavier <bxsr@proton.me>
2025-06-12 17:54:25 -0500

    feat: basic config

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,7 +1,18 @@
 ;;; init.el --- Configuration -*- lexical-binding: t; -*-
 
-(setq inhibit-sturtup-message t)
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(global-display-line-numbers-mode 1)
+(setopt inhibit-splash-screen t)
+
+(when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+(when (fboundp 'global-display-line-numbers-mode)
+  (global-display-line-numbers-mode 1)
+  (setq display-line-numbers-type 'relative))
+
+(load-theme 'modus-vivendi-tritanopia t)
+;; (load-theme 'modus-operandi-tinted t)
+
+(set-face-attribute 'default nil
+                    :height 120
+		    :family "Fira Code")
