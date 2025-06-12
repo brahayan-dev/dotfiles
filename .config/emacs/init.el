b55ea52b805d59e15d65dabf14fe4b6013ac7090
Brahayan Xavier <bxsr@proton.me>
2025-06-12 09:31:02 -0500

    feat: start config

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,1 +1,7 @@
 ;;; init.el --- Configuration -*- lexical-binding: t; -*-
+
+(setq inhibit-sturtup-message t)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(global-display-line-numbers-mode 1)
