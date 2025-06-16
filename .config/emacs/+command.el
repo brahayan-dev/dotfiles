08e3ab6edfe218c20a9dd0f1c53feef5bfd0ddde
Xavier Ramirez <xavier.ramirez@nubank.com.br>
2025-06-16 11:43:54 -0500

    feat: from emacs to neovim

--- .config/emacs/+command.el
+++
@@ -1,6 +0,0 @@
-;;; +base.el --- Custom Functions -*- lexical-binding: t; -*-
-
-(defun load-if-exists (file)
-  "Load `file` if it exists."
-  (when (file-exists-p file)
-    (load file)))
--- .config/emacs/early-init.el
+++
@@ -1,7 +0,0 @@
-;;; early-init.el --- The Early Init File. -*- lexical-binding: t; -*-
-
-;; Use low garbage collection threshold on startup.
-(setq gc-cons-threshold 64000000)
-
-;; Use higher garbage collection threshold after initialization.
-(add-hook 'after-init-hook (lambda () (setq gc-cons-threshold 100000000)))
