45ded085286b07d98f946b5a51530eac667ce392
Brahayan Xavier <bxsr@proton.me>
2025-06-24 07:49:55 -0500

    feat: add new project

--- nvim/init.lua
+++ nvim/init.lua
@@ -690,8 +690,7 @@ require("lazy").setup({
 			end,
 			formatters_by_ft = {
 				lua = { "stylua" },
-				-- Conform can also run multiple formatters sequentially
-				-- python = { "isort", "black" },
+				ruby = { "rubocop" },
 				--
 				-- You can use 'stop_after_first' to run the first available formatter from the list
 				-- javascript = { "prettierd", "prettier", stop_after_first = true },
