fb41f53b383a4ebb14e4ed7bee503418a4441f56
Brahayan Xavier <bxsr@proton.me>
2025-06-28 20:47:55 -0500

    feat: add stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -690,7 +690,6 @@ require("lazy").setup({
 			end,
 			formatters_by_ft = {
 				lua = { "stylua" },
-				ruby = { "rubocop" },
 				--
 				-- You can use 'stop_after_first' to run the first available formatter from the list
 				-- javascript = { "prettierd", "prettier", stop_after_first = true },
