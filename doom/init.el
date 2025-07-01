b9e38652898c2175e839042459132e6e3f3b1fe7
Brahayan Xavier <bxsr@proton.me>
2025-07-01 06:36:29 -0500

    feat: add Bun

--- doom/init.el
+++ doom/init.el
@@ -155,8 +155,8 @@
        ;;       +latexmk)             ; writing papers in Emacs has never been so fun
        ;;lean                         ; for folks with too much to prove
        ;;ledger                       ; be audit you can number-to-string
-       (lua +lsp
-            +tree-sitter)             ; one-based indices? one-based indices
+       ;;(lua +lsp
+       ;;     +tree-sitter)           ; one-based indices? one-based indices
        (markdown +grip)               ; writing docs for people to ignore
        ;;nim                          ; python + lisp at the speed of c
        ;;nix                          ; I hereby declare "nix geht mehr!"
