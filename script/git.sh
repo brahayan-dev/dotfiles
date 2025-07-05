593b942f688f62e451423796b4e209250f6275b2
Brahayan Xavier <bxsr@proton.me>
2025-07-05 05:03:21 -0500

    feat: update config files

--- script/git.sh
+++ script/git.sh
@@ -6,6 +6,8 @@ source "$(dirname "${BASH_SOURCE[0]}")/logging.sh"
 # Connect dotfiles to GitHub
 connect_github() {
     cd ~/.dotfiles || return
+    git remote remove origin
+    git remote add origin git@github.com:brahayan-dev/dotfiles.git
     git remote set-url origin git@github.com:brahayan-dev/dotfiles.git
 }
 
@@ -13,7 +15,7 @@ connect_github() {
 set_ssh_key_to_github() {
     gh auth login
     gh auth refresh -h github.com -s admin:ssh_signing_key
-    gh ssh-key add "$HOME/.ssh/${CURRENT_USER}_rsa.pub" --type signing
+    gh ssh-key add "$HOME/.ssh/${CURRENT_USER}_rsa.pub" --t "${CURRENT_USER}-ssh"
 }
 
 # Execute connect command
