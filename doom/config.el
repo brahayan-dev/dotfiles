2dbcec4996b8d889a5f4870c29b09c405b7fbe72
Your Name <bxsr@proton.me>
2025-07-29 21:50:23 -0500

    feat: make idiomatic

--- doom/config.el
+++ doom/config.el
@@ -13,54 +13,33 @@
 (setq doom-theme 'doom-dracula)
 
 ;; Doom Configuration
-(setq doom-localleader-key ",")
-(setq doom-symbol-font doom-font)
-(setq doom-modeline-buffer-file-name-style 'truncate-all)
+(setq doom-localleader-key ","
+      doom-symbol-font doom-font
+      doom-modeline-buffer-file-name-style 'truncate-all)
 
 ;; Editor Configuration
 (if (fboundp 'toggle-frame-fullscreen) (toggle-frame-fullscreen))
-(setq no-byte-compile nil)
-(setq vterm-shell "/bin/zsh")
-(setq evil-move-beyond-eol t)
-(setq frame-resize-pixelwise t)
-(setq package-native-compile t)
-(setq projectile-enable-caching nil)
-(setq org-directory "~/.dotfiles/doom/")
-(setq display-line-numbers-type 'relative)
-(setq read-process-output-max (* 1024 1024))
-(setq backup-directory-alist `(("." . ,(expand-file-name ".tmp/backups/" user-emacs-directory))))
-(setq projectile-project-root-functions '(projectile-root-local
+(setq no-byte-compile nil
+      vterm-shell "/bin/zsh"
+      evil-move-beyond-eol t
+      frame-resize-pixelwise t
+      package-native-compile t
+      projectile-enable-caching nil
+      org-directory "~/.dotfiles/doom/"
+      display-line-numbers-type 'relative
+      read-process-output-max (* 1024 1024)
+      backup-directory-alist `(("." . ,(expand-file-name ".tmp/backups/" user-emacs-directory)))
+      projectile-project-root-functions '(projectile-root-local
                                           projectile-root-top-down
                                           projectile-root-bottom-up
                                           projectile-root-top-down-recurring))
 
-;; LSP Configuration
 (use-package! lsp-mode
   :commands lsp
   :config
-  (setq lsp-copilot-enabled t)
-  (setq lsp-semantic-tokens-enable t)
-  (setq lsp-inline-completion-idle-delay 1))
-
-;; Hooks
-(add-hook! 'before-save-hook #'whitespace-cleanup)
-(add-hook! 'elfeed-search-mode-hook #'elfeed-update)
-
-(add-hook! 'clojure-mode-hook #'evil-cleverparens-mode)
-(add-hook! 'clojurescript-mode-hook #'evil-cleverparens-mode)
-(add-hook! 'emacs-lisp-mode-hook #'evil-cleverparens-mode)
-
-(add-hook! 'clojure-mode-hook #'smartparens-strict-mode)
-(add-hook! 'clojurescript-mode-hook #'smartparens-strict-mode)
-(add-hook! 'emacs-lisp-mode-hook #'smartparens-strict-mode)
-
-;; Key Mappings
-(map! :v
-      "M-r" #'evil-multiedit-match-all
-      "M-d" #'evil-multiedit-match-and-next
-      "M-D" #'evil-multiedit-match-and-prev)
-
-(after! lsp-mode
+  (setq lsp-copilot-enabled t
+        lsp-semantic-tokens-enable t
+        lsp-inline-completion-idle-delay 1)
   (map! :map lsp-mode-map
         :n "M-l" #'lsp
         :i "C-0" (lambda ()
@@ -68,10 +47,25 @@
                    (corfu-quit)
                    (lsp-inline-completion-display))))
 
-(after! evil-cleverparens
+(use-package! smartparens
+  :hook ((clojure-mode clojurescript-mode emacs-lisp-mode) . smartparens-strict-mode))
+
+(use-package! evil-cleverparens
+  :hook ((clojure-mode clojurescript-mode emacs-lisp-mode) . evil-cleverparens-mode)
+  :config
   (map! :map evil-cleverparens-mode-map
         :n "[[" #'evil-cp-beginning-of-defun
         :n "]]" #'evil-cp-end-of-defun))
+
+;; Hooks
+(add-hook! 'before-save-hook #'whitespace-cleanup)
+(add-hook! 'elfeed-search-mode-hook #'elfeed-update)
+
+;; Key Mappings
+(map! :v
+      "M-r" #'evil-multiedit-match-all
+      "M-d" #'evil-multiedit-match-and-next
+      "M-D" #'evil-multiedit-match-and-prev)
 
 ;; Workspace Configuration
 (when (string= current-workspace "akeptous")
@@ -82,9 +76,8 @@
     (setq doom-font (font-spec :family "Fira Code" :size 18))))
 
 (when (string= current-workspace "work")
-  (setq doom-font (font-spec :family "Fira Code" :size 18))
-  (setq projectile-project-search-path '("~/Projects" "~/dev/nu/"))
-
+  (setq doom-font (font-spec :family "Fira Code" :size 18)
+        projectile-project-search-path '("~/Projects" "~/dev/nu/"))
   (let ((nudev-emacs-path "~/dev/nu/nudev/ides/emacs/"))
     (when (file-directory-p nudev-emacs-path)
       (add-to-list 'load-path nudev-emacs-path)
