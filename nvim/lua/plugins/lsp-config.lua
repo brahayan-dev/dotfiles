1fb391e3f9607f20f0ca28b527669ab3f7076054
Brahayan Xavier <bxsr@proton.me>
2025-09-15 16:04:38 -0500

    refactor: come back to Nvim

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,21 @@
+return {
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+            local lspconfig = require("lspconfig")
+            lspconfig.ruby_lsp.setup({
+                capabilities = capabilities
+            })
+
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>cD", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, {})
+        end,
+    },
+}
---
+++ nvim/lua/settings.lua
@@ -0,0 +1,74 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set softtabstop=2")
+
+-- Set <space> as the leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+
+-- Set to true if you have a Nerd Font installed
+vim.g.have_nerd_font = true
+
+-- [[ Setting options ]]
+
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.mouse = "a"
+
+vim.o.showmode = false
+
+vim.schedule(function()
+    vim.o.clipboard = "unnamedplus"
+end)
+
+vim.o.breakindent = true
+
+vim.o.undofile = true
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.signcolumn = "yes"
+
+vim.o.updatetime = 250
+
+vim.o.timeoutlen = 300
+
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+vim.o.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+vim.o.inccommand = "split"
+
+vim.o.cursorline = true
+
+vim.o.scrolloff = 10
+
+vim.o.confirm = true
+
+vim.opt.swapfile = false
+
+-- Navigate vim panes better
+vim.keymap.set("n", "<leader>w0", "<C-w>q", { desc = "Close current window" })
+vim.keymap.set("n", "<leader>wv", "<C-w>v", { desc = "Split window vertically" })
+vim.keymap.set("n", "<leader>ws", "<C-w>s", { desc = "Split window horizontally" })
+vim.keymap.set("n", "<leader>w<Up>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<leader>w<Down>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<leader>w<Left>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<leader>w<Right>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.hl.on_yank()
+    end,
+})
