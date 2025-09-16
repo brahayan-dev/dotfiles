d503098f857761b5d7db4c48096a924abeb0478d
Brahayan Xavier <bxsr@proton.me>
2025-09-15 19:38:38 -0500

    feat: add support for new repos

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -10,6 +10,7 @@ return {
                 comments = { italic = true },
             },
         })
-        vim.cmd.colorscheme "tokyonight-day"
+        vim.cmd.colorscheme "tokyonight-night"
+        -- vim.cmd.colorscheme "tokyonight-day"
     end
 }
