27f9b2bacc8a11c9c5be3cd7155d02bb188c38cc
Brahayan Xavier <bxsr@proton.me>
2025-09-21 18:04:24 -0500

    feat: add support for Haskell, Elm and Pug

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -4,18 +4,35 @@ return {
         lazy = false,
         config = function()
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
             local lspconfig = require("lspconfig")
+
+            -- Ruby
             lspconfig.ruby_lsp.setup({
                 capabilities = capabilities
             })
 
+            -- Haskell
+            lspconfig.hls.setup({
+                capabilities = capabilities
+            }
+
+            -- Elm
+            lspconfig.elmls.setup({
+                capabilities = capabilities
+            })
+
+            -- PugJS
+            lspconfig.pugls.setup({
+                capabilities = capabilities
+            }))
+
+            -- Keymaps LSP
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, {})
+            vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
             vim.keymap.set("n", "<leader>cc", vim.lsp.buf.references, {})
             vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
             vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-            vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, {})
         end,
     },
 }
