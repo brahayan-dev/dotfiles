0ef0f3d15c69bd1d92de2f446c5097f6789b1194
Brahayan Xavier <bxsr@proton.me>
2025-09-26 00:53:20 -0500

    feat: new config for lsp

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,56 +1,27 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-        lazy = false,
-        config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
-            local lspconfig = require("lspconfig")
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      vim.lsp.config('*', { capabilities = { general = { positionEncodings = { "utf-16" } } } })
 
-            if vim.env.WORKSPACE == "akeptous" then
-              -- Ruby
-              lspconfig.ruby_lsp.setup({
-                  capabilities = capabilities
-              })
+      if vim.env.WORKSPACE == "akeptous" then
+        vim.lsp.enable("ruby_lsp")
+        vim.lsp.enable("elmls")
+      end
 
-              -- Elm
-              lspconfig.elmls.setup({
-                  capabilities = capabilities
-              })
+      vim.lsp.enable("ts_ls")
+      vim.lsp.enable("yamlls")
+      vim.lsp.enable("bashls")
+      vim.lsp.enable("emmet_ls")
 
-              -- PugJS
-              lspconfig.pugls.setup({
-                  capabilities = capabilities
-              })
-            end
-
-            -- JavaScript / TypeScript
-            lspconfig.tsserver.setup({
-                capabilities = capabilities
-            })
-
-            -- SCSS / CSS
-            lspconfig.cssls.setup({
-                capabilities = capabilities
-            })
-
-            -- Bash + Zsh
-            lspconfig.bashls.setup({
-                filetypes = { "sh", "zsh" },
-                capabilities = capabilities
-            })
-
-            -- Yaml
-            lspconfig.yamlls.setup({
-                capabilities = capabilities
-            })
-
-            -- Keymaps LSP
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, {})
-            vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
-            vim.keymap.set("n", "<leader>cc", vim.lsp.buf.references, {})
-            vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-        end,
-    },
+      -- Keymaps LSP
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>zr", vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "<leader>zf", vim.lsp.buf.format, {})
+      vim.keymap.set("n", "<leader>zz", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>zd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>za", vim.lsp.buf.code_action, {})
+    end
+  }
 }
