cda40e14aed405f4f4fb3447b3c6274a11072edd
Your Name <bxsr@proton.me>
2025-12-26 07:54:00 -0500

    feat: add Astro

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -38,8 +38,9 @@ return {
       -- })
 
       if vim.env.WORKSPACE == "akeptous" then
-        -- vim.lsp.enable("hls")
+        vim.lsp.enable("astro")
         vim.lsp.enable("elmls")
+        -- vim.lsp.enable("hls")
         -- vim.lsp.enable("ruby_lsp")
         -- vim.lsp.enable("terraformls")
       end
