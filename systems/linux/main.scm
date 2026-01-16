3389fbca7926da5151d971d614d791be55cf49db
Xavier Ramírez <xavier.ramirez@nubank.com.br>
2026-01-16 15:31:31 -0500

    feat: add icon proc and symlinks

--- systems/linux/main.scm
+++ systems/linux/main.scm
@@ -1,7 +1,7 @@
 (define-module (systems linux main)
-	#:use-module (ice-9 match)
-	#:use-module (systems common base)
-	#:use-module (systems common interactive))
+  #:use-module (ice-9 match)
+  #:use-module (systems common base)
+  #:use-module (systems common interactive))
 
 (define ansible-config-file "systems/linux/ansible.cfg")
 (define setup-playbook-file "systems/linux/playbook/setup.yml")
@@ -9,17 +9,31 @@
 (define (setup!)
   (playbook! ansible-config-file setup-playbook-file))
 
+(define (icon!)
+  (let* ((home (getenv "HOME"))
+         (icon (string-append home "/.local/share/icons/doom.png"))
+         (desktop-file "/usr/share/applications/emacs.desktop")
+         (sed-pattern (string-append "s|Icon=.*|Icon=" icon "|")))
+    (system* "mkdir" "-p" (string-append home "/.local/share/icons"))
+    (system* "wget"
+             "https://raw.githubusercontent.com/eccentric-j/doom-icon/master/cute-doom/doom.png"
+             "-O" icon)
+    (system* "sudo" "sed" "-i" sed-pattern desktop-file)))
+
+(define (editor!)
+  (begin (doom!) (icon!)))
+
 (define (install!)
   (match (->entity)
-	 ("doom" (doom!))))
+    ("doom" (editor!))))
 
 (define (connect!)
   (match (->entity)
-	 ("github" (github!))))
+    ("github" (github!))))
 
 (match (->command)
-       ("setup" (setup!))
-       ("install" (install!))
-       ("connect" (connect!))
-       ("ping" (ping! ansible-config-file))
-       (_ (display "Command Not Found\n")))
+  ("setup" (setup!))
+  ("install" (install!))
+  ("connect" (connect!))
+  ("ping" (ping! ansible-config-file))
+  (_ (display "Command Not Found\n")))
