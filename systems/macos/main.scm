5a3b3c52396375f87c26ead4a9911b680167a946
Brahayan Xavier <bxsr@proton.me>
2026-01-15 22:26:33 -0500

    feat: add support for macOS

---
+++ systems/macos/main.scm
@@ -0,0 +1,26 @@
+(define-module (systems macos main)
+	#:use-module (ice-9 match)
+	#:use-module (systems common base)
+	#:use-module (systems common interactive))
+
+(define ansible-config-file "systems/macos/ansible.cfg")
+(define setup-playbook-file "systems/macos/playbook/setup.yml")
+
+(define (setup!)
+  (playbook! ansible-config-file setup-playbook-file))
+
+(define (install!)
+  (match (->entity)
+	 ("doom" (doom!))))
+
+(define (connect!)
+  (match (->entity)
+	 ("github" (github!))))
+
+(match (->command)
+       ("setup" (setup!))
+       ("install" (install!))
+       ("connect" (connect!))
+       ("ping" (ping! ansible-config-file))
+       (_ (display "Command Not Found\n")))
+
