37da4d4475ec66032fb32a1ad287f46b9a84508a
Xavier Ramírez <xavier.ramirez@nubank.com.br>
2026-01-19 15:51:15 -0500

    fix: add GH login

--- systems/common/interactive.scm
+++ systems/common/interactive.scm
@@ -1,12 +1,12 @@
 (define-module (systems common interactive)
-       #:export (doom!
-		 github!))
+  #:export (doom!
+            github!))
 
 (define (doom!)
   (let* ((home (getenv "HOME"))
          (emacs (string-append home "/.config/emacs"))
          (doom (string-append emacs "/bin/doom"))
-	 (repo (string-append "git clone --depth 1 https://github.com/doomemacs/doomemacs " emacs)))
+         (repo (string-append "git clone --depth 1 https://github.com/doomemacs/doomemacs " emacs)))
     (system* "rm" "-rf" emacs)
     (system repo)
     (chdir emacs)
@@ -24,6 +24,7 @@
          (ssh-key-path (string-append home "/.ssh/" user "_rsa.pub"))
          (ssh-key-title (string-append user "-ssh")))
     (system* "git" "remote" "set-url" "origin" "git@github.com:brahayan-dev/dotfiles.git")
+    (system* "gh" "auth" "login")
     (system* "gh" "auth" "refresh"
              "-h" "github.com"
              "-s" "admin:ssh_signing_key")
@@ -31,4 +32,3 @@
              ssh-key-path
              "-t"
              ssh-key-title)))
-
