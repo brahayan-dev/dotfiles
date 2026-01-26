8151e9f883a9af200bfd02590708b3ddf8466848
Xavier Ramírez <xavier.ramirez@nubank.com.br>
2026-01-26 15:13:01 -0500

    init: start setup (work)

--- systems/work/main.scm
+++ systems/work/main.scm
@@ -3,6 +3,17 @@
   #:use-module (systems common interactive)
   #:use-module (systems work internal))
 
+(define ansible-config-file "systems/work/ansible.cfg")
+(define setup-playbook-file "systems/work/playbook/setup.yml")
+
+(command (display "Command Not Found\n"))
+
+(command 'ping
+         (ping! ansible-config-file))
+
+(command 'setup
+         (playbook! ansible-config-file setup-playbook-file))
+
 (command (display "Command Not Found\n"))
 
 (command 'refresh 'nu
