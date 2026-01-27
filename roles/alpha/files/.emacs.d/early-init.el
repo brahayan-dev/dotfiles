970348fef5f038bb5906fc825b85a54d58477d16
brahayan-dev <73149340+brahayan-dev@users.noreply.github.com>
2026-01-26 23:55:55 -0500

    refactor: update early-init.el

--- roles/alpha/files/.emacs.d/early-init.el
+++ roles/alpha/files/.emacs.d/early-init.el
@@ -1,21 +1,40 @@
-;;; early-init.el --- The Early Init File. -*- lexical-binding: t; -*-
+;;; early-init.el --- Early initialization -*- lexical-binding: t; -*-
 
-(setq gc-cons-threshold most-positive-fixnum)
+;;;; ------------------------------------------------------------
+;;;; Performance (early)
+;;;; ------------------------------------------------------------
 
-;; Disable GUI elements early
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6)
+
+;;;; ------------------------------------------------------------
+;;;; Disable GUI elements early
+;;;; ------------------------------------------------------------
+
 (add-to-list 'default-frame-alist '(horizontal-scroll-bars . nil))
 (add-to-list 'default-frame-alist '(vertical-scroll-bars . nil))
+(add-to-list 'default-frame-alist '(menu-bar-lines . 0))
+(add-to-list 'default-frame-alist '(tool-bar-lines . 0))
 
-(if (fboundp 'horizontal-scroll-bar-mode) (horizontal-scroll-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'menu-bar-mode)  (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode)  (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(when (fboundp 'horizontal-scroll-bar-mode)
+  (horizontal-scroll-bar-mode -1))
+
 (setq inhibit-startup-message t)
+(setq inhibit-startup-screen t)
 
-(setq custom-file (expand-file-name "~/.emacs.d/custom.el"))
-(when (file-exists-p custom-file)
-  (load custom-file))
+;;;; ------------------------------------------------------------
+;;;; Fonts / GC internals
+;;;; ------------------------------------------------------------
 
 (setq inhibit-compacting-font-caches t)
 
+;;;; ------------------------------------------------------------
+;;;; Package system (delegated to init.el)
+;;;; ------------------------------------------------------------
+
 (setq package-enable-at-startup nil)
+
+;;; early-init.el ends here
