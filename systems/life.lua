c67cf81797322fb21e6b5fa63d2f370ca5da1faf
Brahayan Xavier <bxsr@proton.me>
2026-02-02 21:18:41 -0500

    feat: make idiomatic

--- systems/life.lua
+++ systems/life.lua
@@ -1,71 +1,76 @@
 local shell = require "systems.library.common".shell
 local set_ansible_cfg = require "systems.library.common".set_ansible_cfg
 
-local vault_file = "systems/.vault_"
-local become_file = "systems/.become_"
-local hosts_file = "systems/hosts.ini"
-local ansible_cfg_file = "systems/life.cfg"
-local setup_playbook_file = "systems/life.yml"
+local paths = {
+  vault_file = "systems/.vault_",
+  become_file = "systems/.become_",
+  hosts_file = "systems/hosts.ini",
+  ansible_cfg_file = "systems/life.cfg",
+  setup_playbook_file = "systems/life.yml"
+}
 
 local not_found = function(_)
   print("Command Not Found\n")
 end
 
-local connect = function()
-  local parts__connect_dotfiles_repo = {
+local set_repository_origin = function()
+  shell {
     "git", "remote",
-    "set-url", "origin", "git@github.com:brahayan-dev/dotfiles.git"
+    "set-url", "origin",
+    "git@github.com:brahayan-dev/dotfiles.git"
   }
-  shell(parts__connect_dotfiles_repo)
+end
 
-  local parts__make_login = {
+local authenticate_github = function()
+  shell {
     "gh", "auth", "login"
   }
-  shell(parts__make_login)
+end
 
-  local parts__refresh_toke = {
+local refresh_token = function()
+  shell {
     "gh", "auth", "refresh",
     "-h", "github.com",
     "-s", "admin:ssh_signing_key"
   }
-  shell(parts__refresh_toke)
+end
 
+local set_ssh_key = function()
   local home = os.getenv("HOME")
   local host = os.getenv("HOST")
   local user = os.getenv("USER")
-  local parts__set_ssh_key = {
+
+  shell {
     "gh", "ssh-key", "add",
-    home .. "/.ssh/" .. user .. "_rsa.pub",
-    "-t", "Ak " .. host
+    string.format("%s/.ssh/%s_rsa.pub", home, user),
+    string.format("-t Ak %s", host),
   }
-  shell(parts__set_ssh_key)
+end
+
+local connect = function(_)
+  set_repository_origin()
+  authenticate_github()
+  refresh_token()
+  set_ssh_key()
 end
 
 local setup = function(_)
-  local parts = {
-    set_ansible_cfg(ansible_cfg_file),
-    "ansible-playbook",
-    "-c local",
-    "-i " .. hosts_file,
-    "--vault-password-file " .. vault_file,
-    "--become-password-file " .. become_file,
-    setup_playbook_file
+  shell {
+    set_ansible_cfg(paths.ansible_cfg_file),
+    "ansible-playbook", "-c", "local",
+    string.format("-i %s", paths.hosts_file),
+    string.format("--vault-password-file %s", paths.vault_file),
+    string.format("--become-password-file %s", paths.become_file),
+    paths.setup_playbook_file
   }
-
-  shell(parts)
 end
 
 local ping = function(_)
-  local parts = {
-    set_ansible_cfg(ansible_cfg_file),
-    "ansible",
-    "-i " .. hosts_file,
-    "Workstation",
-    "-c local",
-    "-m ping"
+  shell {
+    set_ansible_cfg(paths.ansible_cfg_file),
+    "ansible", "-c", "local", "-m", "ping",
+    string.format("-i %s Workstation", paths.hosts_file)
   }
-
-  shell(parts)
 end
 
 local commands = {
